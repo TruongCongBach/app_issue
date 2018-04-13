4267d2efaaab7fd027c4a78e80e1eea9e6caa5d7
TruongCongBach <truongcongbach1996@gmail.com>
2018-04-13 13:47:23 +0700

    aadd ajax

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -3,22 +3,15 @@ $(document).ready(function () {
        url: '/showIssueNews',
         method: 'get',
         contentType: 'application/json' 
-    }).then(renderNews);
+    }).then(renderListIssue);
 
-    function renderNews(issue) {
-        let template = $('#table-new-template').html();
-        let resultHtml = issue.map(function (issue) {
-            return template
-                .replace(':issue:', issue.content)
-        }).join('');
-        $('#table-news').html(resultHtml);
-    }
+
+
     $.ajax({
        url: '/listTopic',
        method: 'get',
        contentType: 'application/json'
-
-   }).then(renderTopic);
+    }).then(renderTopic);
 
     $(document).on('click', '#a', function () {
         $.ajax({
@@ -28,11 +21,16 @@ $(document).ready(function () {
         }).then(renderListIssue);
     });
 
+
+
+
+
     function renderListIssue(arrayIssue) {
         let template = $('#list-issue-template').html();
         let resultHtml = arrayIssue.map(function (issue) {
             return template
                 .replace(':Issue:', issue.content)
+                .replace(':issue-id:', issue.id)
         }).join('');
         $('#table-news').html(resultHtml);
     }
--- src/comment/index.js
+++ src/comment/index.js
@@ -1,7 +1,7 @@
 const CommentRepo       = require('../comment/repo-comment');
 const FactoryComment    = require('../commentator/make-from-db');
 const ProviderComment   = require('../issue/search-services/provider-comment');
-module.exports = function (app, connection) {
-    app.set('comment.repo', new CommentRepo(connection));
-    app.set('listComment', new ProviderComment(connection, new FactoryComment()));
+module.exports = function (app) {
+    app.set('comment.repo', new CommentRepo(app.get('databaseConnection')));
+    app.set('listComment', new ProviderComment(app.get('databaseConnection'), new FactoryComment()));
 };
